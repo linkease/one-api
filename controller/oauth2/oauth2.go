3c637ebb2327761ba2c1d4795dc2d96141e77d30
Carseason <757841203@qq.com>
2025-11-04 11:22:55 +0800

    update: defaultUserQuota = 100

--- controller/oauth2/oauth2.go
+++ controller/oauth2/oauth2.go
@@ -135,7 +135,7 @@ func insertKoolcenterUser(ctx context.Context, userinfo *Userinfo) (*model.User,
 		user.Role = model.RoleCommonUser
 		user.Status = model.UserStatusEnabled
 		// 默认额度
-		// user.Quota = 10000
+		user.Quota = 100
 		err = user.Insert(ctx, 0)
 	}
 	if err != nil {
